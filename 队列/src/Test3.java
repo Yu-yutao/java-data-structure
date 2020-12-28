d73846e2a75b8b360f50fab1f1547a025c490ba4
Yu-yutao <2549326724@qq.com>
2020-12-28 09:55:45 +0800

    双端队列

---
+++ 队列/src/Test3.java
@@ -0,0 +1,19 @@
+public class Test3 {
+    public static void main(String[] args) {
+        // 在下面的双端队列的测试中，偶数从队尾加入；奇数从队首加入
+        Deque<Integer> dq = new Deque<>();
+        for(int i = 0 ; i < 16 ; i ++){
+            if(i % 2 == 0) dq.addLast(i);
+            else dq.addFront(i);
+            System.out.println(dq);
+        }
+
+        // 之后，我们依次从队首和队尾轮流删除元素
+        System.out.println();
+        for(int i = 0; !dq.isEmpty(); i ++){
+            if(i % 2 == 0) dq.removeFront();
+            else dq.removeLast();
+            System.out.println(dq);
+        }
+    }
+}
