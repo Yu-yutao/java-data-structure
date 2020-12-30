5bea6e91879c560b856438cc27c5a7d7e27eede9
Yu-yutao <2549326724@qq.com>
2020-12-30 10:35:04 +0800

    save

---
+++ leetcode链表题/src/Solution.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) { val = x; }
+ * }
+ */
+
+public class Solution {
+    public ListNode deleteNode(ListNode head, int val) {
+        while (head != null && head.val == val) {
+           head = head.next;
+        }
+
+        if (head == null) {
+            return null;
+        }
+
+        ListNode prev = head;
+        while (prev.next != null) {
+            if (prev.next.val == val) {
+                prev.next = prev.next.next;
+            } else {
+                prev = prev.next;
+            }
+        }
+
+        return head;
+    }
+}
